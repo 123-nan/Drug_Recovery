39a306bd4556c069a7aa94d3309022392e8eaecf
Akriti <akritisingh1811@gmail.com>
2022-05-21 23:29:54 +0530

    home page

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <>
+    
+    <div className="background pt-3 ">
+        <div className=" col-md-8 heading mx-auto my-5 p-5 ">
+            <h1 className="headinghome">THE MOST VALUABLE </h1><br/>
+            <h1 className="headinghome">THING IS YOUR HEALTH</h1>
+        </div>    
+    </div>
+    
+    </>
+  )
+}
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,71 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Navbar.css'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <>
+  <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{props.title}</a>
+    
+    <a className="navbar-brand ml-5" href="/">{props.title}</a>
+    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+
+      <ul className="navbar-nav mx-auto mb-2 mb-lg-0 px-2">
+
+        <li className="nav-item ">
           <a className="nav-link active color:white" aria-current="page" href="/">Home</a>
         </li>
+
         <li className="nav-item">
           <a className="nav-link" href="/">About</a>
         </li>
         
+        <li className="nav-item">
+          <a className="nav-link" href="/">Therapists</a>
+        </li>
+        
+        <li className="nav-item">
+          <a className="nav-link" href="/">Blog</a>
+        </li>
+        
+        <li className="nav-item ">
+          <a className="nav-link" href="/">Report</a>
+        </li>
+        
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+
+      <ul className="nav justify-content-end">
+        <li className="nav-item nav-link" style={{color: "brown"}}>Login</li>
+               
+        <li className="nav-item nav-link" style={{color: "brown"}}>SignUp</li>
+              
+                 {/* {user&& <div class="dropdown">
+                    <button class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Profile
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-lg-end" aria-labelledby="dropdownMenuButton1">
+                    <Link to='/profile'>
+                        <li className="dropdown-item">My Profile</li>
+                    </Link>
+                    <Link to='/dailyintake'>
+                        <li className="dropdown-item">Report</li>
+                    </Link>
+                        <li className="dropdown-item" onClick={logOutHandler}>LogOut</li>
+                    </ul>
+                    </div>} */}
+
+        </ul>
+      
     </div>
   </div>
 </nav>
+</>
   )
 }
 
