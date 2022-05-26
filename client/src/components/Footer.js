879467c33af2f4842e5dcb882095bc2f5c0e1e89
Akriti <akritisingh1811@gmail.com>
2022-05-26 17:40:05 +0530

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,24 +3,49 @@ import '../styles/Footer.css'
 
 export default function Footer() {
   return (
-    <div className="footer p-1">
-        <h1 className='tophead m-5'>Cure Street</h1>
-       <div className='m-5'>
-        <ul class="nav justify-content-center">
-            <li class="footeritem">
-                <a class="nav-link footeritem" href="/">HOME</a>
-            </li>
-            <li class="footeritem">
-                <a class="nav-link footeritem" href="/about">ABOUT</a>
-            </li>
-            <li class="footeritem">
-                <a class="nav-link footeritem" href="/testimonial">TESTIMONIAL</a>
-            </li>
-            <li class="footeritem">
-                <a class="nav-link footeritem" href="/contact">CONTACT US</a>
-            </li>
-        </ul>
-        </div>
+    <>
+    
+    <div class="footer-dark">
+        <footer>
+            <div class="containerfooter mx-5">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Features</h3>
+                        <ul>
+                            <li>Therapist Appointment</li>
+                            <li>Chat </li>
+                            <li>Video Call</li>
+                            <li>Generate Report</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li ><a href="/about">About </a></li>
+                            <li ><a href="/testimonial">Testimonial</a></li>
+                            <li ><a href="/contact">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Cure Street</h3>
+                        <p>Founded in 2022,Cure Street is an on-demand,digital primary healthcare platform which offers professional services that can be availed from the comfort of home,office or anywhere you are comforatble at.
+                            Cure Street's endeavour has always been to make good health more accessible,reliable and hassle-free to all.</p>
+                    </div>
+                    <div class="col item social">
+                        <a href="/"><i class="icon ion-social-facebook"></i></a>
+                        <a href="/"><i class="icon ion-social-twitter"></i></a>
+                        <a href="/"><i class="icon ion-social-snapchat"></i></a>
+                        <a href="/"><i class="icon ion-social-instagram"></i></a>
+                        </div>
+                </div>
+                <p class="copyright">Cure Street © 2022</p>
+            </div>
+        </footer>
     </div>
+    
+    
+
+    </>
   )
 }
+
